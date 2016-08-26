e53137f69494734806cd5c23eccb608789f22590
Brad Runnels <runnels.brad@gmail.com>
2016-08-26 10:13:59 -0400

    base index and working main.js - will branch going forward

--- js/main.js
+++ js/main.js
@@ -13,126 +13,138 @@
  */
 
 
-const controller = function customizationProcess() {
 
-    return {
-        next() { step = step === 5 ? step = 5 : step += 1; },
-        previous() { step = step === 1 ? step = 1 : step -= 1; },
-        getStep() { return step; },
-        
-        setTypeface() { typefaceSet = true; },
-        isTypefaceSet() { return typefaceSet; }
-    };
-}(step = 1, typefaceSet = false);
+    const controller = function customizationProcess() {
 
+        let step = 1;
+        let typefaceSet = false;
 
-// STEP 1: Select the type of box to be customized
-const step1 = function selectBox() {
-
-    const boxSelection = document.getElementById('step-1__select'),
-          boxImg   = document.getElementById('step-1__image'),
-          boxPrice = document.getElementById('box__price'),
-          boxDesc  = document.getElementById('box__desc');
+        return {
+            next() { step = step === 5 ? step = 5 : step += 1; },
+            previous() { step = step === 1 ? step = 1 : step -= 1; },
+            getStep() { return step; },
+            
+            setTypeface() { typefaceSet = true; },
+            isTypefaceSet() { return typefaceSet; }
+        };
+    }();
 
 
-    const box = {
-        ammo: {
-            name: 'Ammo Box',
-            image: "{{ 'ammo.jpg' | asset_url }}",
-            price: 109.99,
-            desc: 'Ammo Box description goes here'
-        },
-        artillery: {
-            name: 'Artillery Box',
-            image: "{{ 'artillery.jpg' | asset_url }}",
-            price: 119.99,
-            desc: 'Artillery Box description goes here'
-        },
-        traditional: {
-            name: 'Traditional Box',
-            image: "{{ 'traditional.jpg' | asset_url }}",
-            price: 115.99,
-            desc: 'Traditional Box description goes here'
-        },
-        letter: {
-            name: 'Letter Box',
-            image: "{{ 'letter.jpg' | asset_url }}",
-            price: 95.99,
-            desc: 'Letter Box description goes here'
-        },
-    };
+    // STEP 1: Select the type of box to be customized
+    const step1 = function selectBox() {
+
+        const boxSelection = document.getElementById('step-1__select'),
+              boxImg   = document.getElementById('step-1__image'),
+              boxPrice = document.getElementById('box__price'),
+              boxDesc  = document.getElementById('box__desc');
 
 
-    function boxDefault(boxType) {
+        const box = {
+            ammo: {
+                name: 'Ammo Box',
+                image: "{{ 'ammo.jpg' | asset_url }}",
+                price: 109.99,
+                desc: 'Ammo Box description goes here'
+            },
+            artillery: {
+                name: 'Artillery Box',
+                image: "{{ 'artillery.jpg' | asset_url }}",
+                price: 119.99,
+                desc: 'Artillery Box description goes here'
+            },
+            traditional: {
+                name: 'Traditional Box',
+                image: "{{ 'traditional.jpg' | asset_url }}",
+                price: 115.99,
+                desc: 'Traditional Box description goes here'
+            },
+            letter: {
+                name: 'Letter Box',
+                image: "{{ 'letter.jpg' | asset_url }}",
+                price: 95.99,
+                desc: 'Letter Box description goes here'
+            },
+        };
 
-        // TODO: should add default to select so that it references the same box type
-        // boxSelection.options[boxSelection[boxType]].selected = 'selected';
 
-        boxImg.src = box[boxType].image;
-        boxImg.alt = box[boxType].name;
-        boxPrice.textContent = box[boxType].price;
-        boxDesc.textContent = box[boxType].desc;
-    }
-
-
-    // set default box type
-    boxDefault('ammo');
-
-
-    boxSelection.onchange = function () {
-
-        let boxOption = boxSelection.options[boxSelection.selectedIndex].value;
-
-        // if unrecognized box type, set default
-        if (box[boxOption] === undefined) {
-            boxDefault('ammo'); 
-        } else {
-            boxImg.src = box[boxOption].image;
-            boxImg.alt = box[boxOption].name;
-            boxPrice.textContent = box[boxOption].price;
-            boxDesc.textContent = box[boxOption].desc;
+        function displaySelectedBox(boxType) {
+            boxImg.src = box[boxType].image;
+            boxImg.alt = box[boxType].name;
+            boxPrice.textContent = box[boxType].price;
+            boxDesc.textContent = box[boxType].desc;
         }
-    };
-
-    // boxDefault should be an accessible method
-    return {
-        boxDefault
-    };
-}();
 
 
+        // set default box type
+        displaySelectedBox('ammo');
 
-// STEP 2: Customize the top of the message box
-const step2 = function customizeTop() {
 
-    const topSelection = document.getElementById('step-2__select');
+        boxSelection.onchange = function () {
 
-    const options = {
-        none: 'no customization for step 2',
-        text: function () {
-            // show textarea for custom text
-        },
-        icon: function () {
-            // show icon upload button
-        },
-        quote_1: 'Quote 1 goes here',
-        quote_2: 'Quote 2 goes here',
-        quote_3: 'Quote 3 goes here',
-        quote_4: 'Quote 4 goes here',
-        verse_1: 'Verse 1 goes here',
-        verse_2: 'Verse 2 goes here',
-        verse_3: 'Verse 3 goes here',
-        verse_4: 'Verse 4 goes here'
-    };
+            let boxOption = boxSelection.options[boxSelection.selectedIndex].value;
 
-    topSelection.onchange = function () {
+            // if unrecognized box type, set default
+            if (box[boxOption] === undefined) {
+                displaySelectedBox('ammo'); 
+            } else {
+                displaySelectedBox(boxOption);
+            }
+        };
 
-        const topOption = topSelection.options[topSelection.selectedIndex].value;
-        
-        // code to execute when option is selected
-    };
+        // displaySelectedBox should be an accessible method
+        return {
+            // displaySelectedBox()
+        };
+    }();
 
-}();
+
+
+    // STEP 2: Customize the top of the message box
+    const step2 = function customizeTop() {
+
+        const topSelection  = document.getElementById('step-2__select'),
+              topTypeface   = document.getElementById('step-2__typeface'),
+              topCustomText = document.getElementById('step-2__custom-text'),
+              topStockText  = document.getElementById('step-2__stock-text'),
+              topIcon       = document.getElementById('step-2__upload-icon');
+
+        const options = {
+            none: 'no customization for step 2',
+            text: function () {
+                topCustomText.classList.remove('hidden');
+            },
+            icon: function () {
+                // show icon upload button
+            },
+            quote_1: 'Quote 1 goes here',
+            quote_2: 'Quote 2 goes here',
+            quote_3: 'Quote 3 goes here',
+            quote_4: 'Quote 4 goes here',
+            verse_1: 'Verse 1 goes here',
+            verse_2: 'Verse 2 goes here',
+            verse_3: 'Verse 3 goes here',
+            verse_4: 'Verse 4 goes here'
+        };
+
+        function displayTopSelection(option) {
+            // if unrecognized option, reset select
+            if (options[option] === undefined) {
+                topSelection.value = '';
+                return undefined;
+            } else {
+
+            }
+
+        }
+
+        topSelection.onchange = function () {
+
+            let topOption = topSelection.options[topSelection.selectedIndex].value;
+            
+            displayTopSelection(stopPropagation())
+        };
+
+    }();
 
 /*
     // image loading functionality
