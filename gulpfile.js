ba3edb421988aa6771536383d716db6321be7d59
Brad Runnels <runnels.brad@gmail.com>
2016-08-31 22:02:53 -0400

    taskrunner

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+
+const gulp         = require('gulp'),
+    htmlmin      = require('gulp-htmlmin'),
+    sass         = require('gulp-sass'),
+    gulpBrowser  = require("gulp-browser"),
+    babel        = require('gulp-babel'),
+    pump         = require('pump'),
+    uglify       = require('gulp-uglify');
+
+
+gulp.task('html', function () {
+    return gulp.src('./index.html')
+    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .pipe(gulp.dest('../public/'));
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('../public/css/'));
+});
+
+
+gulp.task('js', function (e) {
+    pump([
+        gulp.src('./js/main.js'),
+        // gulpBrowser.browserify(),
+        babel({ presets: ['es2015'] }),
+        uglify(),
+        gulp.dest('../public/js/')
+    ], e);
+});
+
+
+gulp.task('img', function () {
+    return gulp.src('./img/*')
+    .pipe(gulp.dest('../public/img/'));
+});
+
+
+// ====================================================
+// run 'default' gulp cmd to perform an array of tasks!
+// ====================================================
+
+gulp.task('default', [ 'html', 'sass', 'js', 'img' ]);
+
+gulp.task('watch', function () {
+    gulp.watch('./*html', ['html']);
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./js/*.js', ['js']);
+});
+
+// call exclusively to compile scss without full build
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/*.scss', ['sass']);
+});
--- js/main.js
+++ js/main.js
@@ -8,105 +8,16 @@
  *  NEEDS:
  *  - validate each step so that user cannot progress until selection has been made
  *  - maybe clear unused form elements before submission
- *  - incorporate final assets (obvious)
- *  - incorporate into Shopify (obvious)
- *  - add image upload funcitonality w/cropping
- *  - pass selected text as a formatted image
+ *  - add image upload functionality w/cropping <== use croppie
+ *  - pass selected text as a formatted image and display final product to user for proofing purposes
+ *  - gift options for step 5
+ *  - stock icon options only - no user uploaded icons
+ *  - stock quotes provided step 4
+ *  - need to identify persons that have funded on Kickstarter to give discount when they custom box
+ *  - live hooks into Shopify for Kickstarter
+ 
+ *  - way to handle 50 state outline stock option
  */
-
-    const controller = function () {
-        let step = 1;
-        let typefaceSet = false;
-
-        const step1 = document.getElementById('step-1');
-        const step2 = document.getElementById('step-2');
-        const step3 = document.getElementById('step-3');
-        const step4 = document.getElementById('step-4');
-        const step5 = document.getElementById('step-5');
-
-        const steps = [ step1, step2, step3, step4, step5 ];
-
-        const indicator1 = document.getElementById('progress-indicator__step-1');
-        const indicator2 = document.getElementById('progress-indicator__step-2');
-        const indicator3 = document.getElementById('progress-indicator__step-3');
-        const indicator4 = document.getElementById('progress-indicator__step-4');
-        const indicator5 = document.getElementById('progress-indicator__step-5');
-
-        const indicators = [ indicator1, indicator2, indicator3, indicator4, indicator5 ];
-
-        return {
-            nextBtn: document.getElementById('next'),
-            previousBtn: document.getElementById('previous'),
-            addToCartBtn: document.getElementById('add-cart'),
-
-            nextStep() { step = step === 5 ? step = 5 : step += 1; },
-            previousStep() { step = step === 1 ? step = 1 : step -= 1; },
-            getStep() { return step; },
-
-            progressUI() {
-                let state = this.getStep();
-                
-                if (state === 1) {
-                    this.toggleOptions(steps, step1);
-                    this.toggleIndicator(indicators, indicator1);
-                }
-                else if (state === 2) {
-                    this.toggleOptions(steps, step2);
-                    this.toggleIndicator(indicators, indicator2);
-                }
-                else if (state === 3) {
-                    this.toggleOptions(steps, step3);
-                    this.toggleIndicator(indicators, indicator3);
-                }
-                else if (state === 4) {
-                    this.toggleOptions(steps, step4);
-                    this.toggleIndicator(indicators, indicator4);
-                }
-                else if (state === 5) {
-                    this.toggleOptions(steps, step5);
-                    this.toggleIndicator(indicators, indicator5);
-                }
-
-            },
-            
-            setTypeface() { typefaceSet = true; },
-            unsetTypeface() { typefaceSet = false; },
-            isTypefaceSet() { return typefaceSet; },
-
-            toggleOptions(optArr, opt) {
-                optArr.map((e) => {
-                    if (e.className.indexOf('hidden') === -1) {
-                        e.className += 'hidden';
-                    }
-                });
-
-                if (typeof opt !== 'undefined') {
-                    opt.className = opt.className.replace('hidden', '');
-                }
-            },
-            toggleOptionOn(opt) {
-                if (opt.className.indexOf('hidden') > -1) {
-                    opt.className = opt.className.replace('hidden', '');
-                }
-            },
-            toggleOptionOff(opt) {
-                if (opt.className.indexOf('hidden') === -1) {
-                    opt.className += 'hidden';
-                }
-            },
-            toggleIndicator(indArr, ind) {
-                indArr.map((e) => {
-                    if (e.className.indexOf(' progress-indicator__step--highlight') > -1) {
-                        e.className = e.className.replace(' progress-indicator__step--highlight', '');
-                    }
-                });
-
-                ind.className += ' progress-indicator__step--highlight';
-            }
-        };
-    }();
-
-
 
 
     // STEP 1: Select the type of box to be customized
@@ -262,7 +173,7 @@
 
             isTypefaceSet() {
                 return typefaceStep2;
-            }
+            },
         };
     }();
 
@@ -473,6 +384,123 @@
 
 
 
+    const controller = function (stp2, stp3, stp4, stp5) {
+        let step = 1;
+        let typefaceSet = false;
+
+        const step1 = document.getElementById('step-1');
+        const step2 = document.getElementById('step-2');
+        const step3 = document.getElementById('step-3');
+        const step4 = document.getElementById('step-4');
+        const step5 = document.getElementById('step-5');
+
+        const steps = [ step1, step2, step3, step4, step5 ];
+
+        const indicator1 = document.getElementById('progress-indicator__step-1');
+        const indicator2 = document.getElementById('progress-indicator__step-2');
+        const indicator3 = document.getElementById('progress-indicator__step-3');
+        const indicator4 = document.getElementById('progress-indicator__step-4');
+        const indicator5 = document.getElementById('progress-indicator__step-5');
+
+        const indicators = [ indicator1, indicator2, indicator3, indicator4, indicator5 ];
+
+        return {
+            nextBtn: document.getElementById('next'),
+            previousBtn: document.getElementById('previous'),
+            addToCartBtn: document.getElementById('add-cart'),
+
+            error: document.getElementById('selection-error'),
+
+            nextStep() { step = step === 5 ? step = 5 : step += 1; },
+            previousStep() { step = step === 1 ? step = 1 : step -= 1; },
+            getStep() { return step; },
+
+            progressUI() {
+                let state = this.getStep();
+                
+                if (state === 1) {
+                    this.toggleOptions(steps, step1);
+                    this.toggleIndicator(indicators, indicator1);
+                    this.toggleOptionOff(this.previousBtn);
+                }
+                else if (state === 2) {
+                    this.toggleOptions(steps, step2);
+                    this.toggleIndicator(indicators, indicator2);
+                    this.toggleOptionOn(this.previousBtn);
+                }
+                else if (state === 3) {
+                    this.toggleOptions(steps, step3);
+                    this.toggleIndicator(indicators, indicator3);                    
+                }
+                else if (state === 4) {
+                    this.toggleOptions(steps, step4);
+                    this.toggleIndicator(indicators, indicator4);
+                    this.toggleOptionOff(this.addToCartBtn);
+                    this.toggleOptionOn(this.nextBtn);
+                }
+                else if (state === 5) {
+                    this.toggleOptions(steps, step5);
+                    this.toggleIndicator(indicators, indicator5);
+                    this.toggleOptionOff(this.nextBtn);
+                    this.toggleOptionOn(this.addToCartBtn);
+                }
+
+            },
+            
+            setTypeface() { typefaceSet = true; },
+            unsetTypeface() { typefaceSet = false; },
+            isTypefaceSet() { return typefaceSet; },
+
+            toggleOptions(optArr, opt) {
+                optArr.map((e) => {
+                    if (e.className.indexOf('hidden') === -1) {
+                        e.className += 'hidden';
+                    }
+                });
+
+                if (typeof opt !== 'undefined') {
+                    opt.className = opt.className.replace('hidden', '');
+                }
+            },
+            toggleOptionOn(opt) {
+                if (opt.className.indexOf('hidden') > -1) {
+                    opt.className = opt.className.replace('hidden', '');
+                }
+            },
+            toggleOptionOff(opt) {
+                if (opt.className.indexOf('hidden') === -1) {
+                    opt.className += 'hidden';
+                }
+            },
+            toggleIndicator(indArr, ind) {
+                indArr.map((e) => {
+                    if (e.className.indexOf(' progress-indicator__step--highlight') > -1) {
+                        e.className = e.className.replace(' progress-indicator__step--highlight', '');
+                    }
+                });
+
+                ind.className += ' progress-indicator__step--highlight';
+            },
+
+            validateStep(select) {
+                if (select.value === '_') {
+                    this.error.textContent = 'Error: Please make a selection.';
+                    return false;
+                }
+                // else if (select.value === 'text' && textOption === '') {
+                //     this.error.textContent = 'Error: Please enter your custom text.';
+                //     return false;
+                // }
+                else {
+                    return true;
+                }
+            }
+        };
+    }(step2, step3, step4, step5);
+
+
+
+
     const form = document.getElementById('customization-form');
 
     // customization form listener for select fields
@@ -506,10 +534,12 @@
         if (e.target === controller.nextBtn) {
             controller.nextStep();
             controller.progressUI();
+            console.log('You\'re on step: ' + controller.getStep());
         }
         else if (e.target === controller.previousBtn) {
             controller.previousStep();
             controller.progressUI();
+            console.log('You\'re on step: ' + controller.getStep());
         }
 
         e.stopPropagation();
