93c01a940fa044b5f415c89ddf369375e542ee97
Clay Griswold <ccgriswold@gmail.com>
2016-10-12 14:28:51 -0400

    Tweaks to make image search from Assets instead of img/ Other minor updates across html and scss files

--- js/boxes.js
+++ js/boxes.js
@@ -1,7 +1,7 @@
 const boxes = {
     ammo: {
         name: 'Ammo Box',
-        image: "https://www.dropbox.com/s/tj601wqf4sq2n5d/Dad%20Son%20Fishing%20exterior.jpg?raw=1",
+        image: "Assets/DadSonFishingexterior.png",
         price: 109.99,
         desc: 'Hand-crafted with American pride and care, the SF Ammo Box is a rustic throw-back to when Davy Crockett was still King of the Wild Frontier.',
         length: 8.25,
@@ -10,7 +10,7 @@ const boxes = {
     },
     artillery: {
         name: 'Artillery Box',
-        image: "https://www.dropbox.com/s/e0fg3wi7s99u3b9/Wedding%20Exterior.jpg?raw=1",
+        image: "Assets/SouthernFavorExterior.png",
         price: 119.99,
         desc: 'The SF Artillery Box is hand-crafted with American hardwood and leather. While it looks like it might have fallen off of a pirate\'s ship, it\'s refined and regal enough to sit on your favorite buccaneer\'s mantle.',
         length: 8.25,
@@ -19,7 +19,7 @@ const boxes = {
     },
     traditional: {
         name: 'Buckshot Box',
-        image: "https://www.dropbox.com/s/iunuckgktfcq9mw/Ryan%20Exterior.jpg?raw=1",
+        image: "Assets/RyanExterior.png",
         price: 115.99,
         desc: 'While they may not use it to lug around buckshot, this carefully crafted, petite box is perfect for that special Southern Bell (or Buck) in your life.',
         length: 5.75,
@@ -28,7 +28,7 @@ const boxes = {
     },
     letter: {
         name: 'Letter Box',
-        image: "https://www.dropbox.com/s/g28303byzvomgd7/Daddy%20Letter%20box%20exterior.jpg?raw=1",
+        image: "Assets/DaddyLetterboxexterior.png",
         price: 95.99,
         desc: 'The hand-written letter is a fading art, so we brought is back and made it 100X\'s more meaningful. Perfect for delivering the hand-written letter and SF coasters, the Letter Box is sure to store trinkets and treasures on dressers for years to come.',
         length: 5.75,
@@ -40,7 +40,7 @@ const boxes = {
 function reset(box) {
     // Copy the box so we never actually dirty the original.
     box = Object.assign({}, box);
-    
+
     box.custom = {
         content: null, // either 'text' or 'image'
         font: null,
